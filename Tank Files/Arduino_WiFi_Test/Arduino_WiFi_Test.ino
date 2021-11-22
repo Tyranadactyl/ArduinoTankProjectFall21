5128cb0cc80476ef80977acad070835af9397f3b
Jonathan <jonathanmistishin@gmail.com>
2021-11-22 11:57:03 -0500

    Updated comments and new feature
    Added the fail safe that stops the tank if it looses wifi connection

--- Tank Files/Arduino_WiFi_Test/Arduino_WiFi_Test.ino
+++ Tank Files/Arduino_WiFi_Test/Arduino_WiFi_Test.ino
@@ -1,7 +1,7 @@
 #include <SPI.h>
 #include <WiFiNINA.h>
 
-//Establish variables for ease of use
+//Establish pin names for ease of reference
 //R/L (Right/Left) F/R (Forward/Reverse)
 const int RF = 2;
 const int RR = 3;
@@ -12,8 +12,8 @@ const int LR = 5;
 int connectionStatus = WL_IDLE_STATUS;
 
 //Variables for network id and password
-char ssid[] = "Draco";
-char pass[] = "123456789";
+char ssid[] = "ssid";
+char pass[] = "password";
 
 // server is a WiFi server to be hosted on port 80
 WiFiServer server(80);
@@ -28,44 +28,51 @@ void setup() {
   pinMode(LF, OUTPUT);
   pinMode(LR, OUTPUT);
 
-  //Check firmware
+  //Check firmware (if the program SHOULD connect but doesnt, un-comment this and check for updates)
   //if (WiFi.firmwareVersion() < WIFI_FIRMWARE_LATEST_VERSION) {
     //Serial.print("Firmware needs an update.");
   //}
 
-  //While not connected, repeatedly attempt to connect to the network
+  //Connect to the network (if it says connecting more than once, it probably won't connect)
   while (connectionStatus != WL_CONNECTED) {
     Serial.print("Connecting to ");
     Serial.println(ssid);
     connectionStatus = WiFi.begin(ssid, pass);
-    delay(10000); //5 sec delay before trying again
+    delay(10000); //10 sec delay before trying again
   }
 
-  //Start the server
+  //Once connected start the server
   server.begin();
 
   //Print the servers IP address
-  Serial.print("The boards IP is: ");
+  Serial.print("My IP is: ");
   Serial.println(WiFi.localIP());
 }
 
 void loop() {
-  //Client states wether or not we have a connected client
+  //creates a 'client' to send and recieve data
   WiFiClient client = server.available();
 
+  //once there is a client, print to the serial monitor
   if (client) {
     Serial.print("we have connection!");
-    //currentline will store the incoming data from the client
+    
+    //'currentLine' will store the incoming data from the client
     String currentLine = "";
-    //while a client is connected
+
+    //while there is unread data from the client
     while (client.connected()) {
-      //if the client is sending data
+      
+      //if the client has data available to read
       if (client.available()) {
+        
         //read the incoming data
         char input = client.read();
+        
         //write the data on the serial monitor
-        Serial.write(input);
-        //if the client isn't requesting any data, then display the webpage for them
+        Serial.write("Recieving: " + input);
+        
+        //if the client hasn't sent data, display the webpage
         if (input == '\n') {
           if (currentLine.length() == 0) {
             client.println("HTTP/1.1 200 OK");
@@ -83,12 +90,14 @@ void loop() {
             currentLine = "";
           }
         }
+        //if there is input, add it to 'currentLine'
         else if (input != '\r') {
           currentLine += input;
         }
+        //Read 'currentLine' and preform an action accordingly
         //Move forward
         if (currentLine.endsWith("GET /F")) {
-          Serial.print("Recieving F");
+          Serial.print("\nRecieving F\n");
           digitalWrite(RF, HIGH);
           digitalWrite(RR, LOW);
           digitalWrite(LF, HIGH);
@@ -96,7 +105,7 @@ void loop() {
         }
         //Back up
         if (currentLine.endsWith("GET /B")) {
-          Serial.print("Recieving B");
+          Serial.print("\nRecieving B\n");
           digitalWrite(RF, LOW);
           digitalWrite(RR, HIGH);
           digitalWrite(LF, LOW);
@@ -104,7 +113,7 @@ void loop() {
         }
         //Turn left
         if (currentLine.endsWith("GET /L")) {
-          Serial.print("Recieving L");
+          Serial.print("\nRecieving L\n");
           digitalWrite(RF, HIGH);
           digitalWrite(RR, LOW);
           digitalWrite(LF, LOW);
@@ -112,7 +121,7 @@ void loop() {
         }
         //Turn right
         if (currentLine.endsWith("GET /R")) {
-          Serial.print("Recieving R");
+          Serial.print("\nRecieving R\n");
           digitalWrite(RF, LOW);
           digitalWrite(RR, HIGH);
           digitalWrite(LF, HIGH);
@@ -120,7 +129,14 @@ void loop() {
         }
         //Stop
         if (currentLine.endsWith("GET /S")) {
-          Serial.print("Recieving S");
+          Serial.print("\nRecieving S\n");
+          digitalWrite(RF, LOW);
+          digitalWrite(RR, LOW);
+          digitalWrite(LF, LOW);
+          digitalWrite(LR, LOW);
+        }
+        //If the tank looses wifi connection, make it stop moving
+        if (WiFi.status() != WL_CONNECTED){
           digitalWrite(RF, LOW);
           digitalWrite(RR, LOW);
           digitalWrite(LF, LOW);
@@ -128,6 +144,7 @@ void loop() {
         }
       }
     }
+    //remove the client
     client.stop();
     Serial.println("connection terminated");
   }
